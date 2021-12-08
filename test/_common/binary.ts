12adff6594b7112b1155e695563199e738b83252
Max J. Polster <max.polster@id-p.de>
2021-12-08 20:57:04 +0100

    Add binary test helpers

---
+++ test/_common/binary.ts
@@ -0,0 +1,42 @@
+
+export class Binary {
+	public constructor(public readonly hex: string) {}
+
+	public get nodeBuffer(): Buffer {
+		return Buffer.from(this.hex, "hex");
+	}
+
+	public get buffer(): ArrayBuffer {
+		const buffer = this.nodeBuffer;
+		return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+	}
+
+	public get array(): Uint8Array {
+		return new Uint8Array(this.nodeBuffer);
+	}
+
+	public get view(): DataView {
+		return new DataView(this.buffer);
+	}
+}
+
+export function binary(parts: TemplateStringsArray, ...values: (ArrayBufferLike | string)[]): Binary {
+	let str = "";
+	for (let i = 0; i < parts.length; i++) {
+		str += parts[i];
+		if (i < values.length) {
+			const value = values[i];
+			if (typeof value === "string") {
+				str += Buffer.from(value, "utf-8").toString("hex");
+			} else {
+				str += Buffer.from(new Uint8Array(value)).toString("hex");
+			}
+		}
+	}
+	str = str.replace(/#[^\n]*\n/g, "");
+	str = str.replace(/\s+/g, "");
+	if (/[^0-9a-f]/i.test(str)) {
+		throw new TypeError(`invalid hex data: ${str}`);
+	}
+	return new Binary(str);
+}
