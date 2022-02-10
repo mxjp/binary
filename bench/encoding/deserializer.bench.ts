7fff11caf28df6982b2c7bb00ad6ac9dff2b9fe3
Max J. Polster <max.polster@id-p.de>
2022-02-10 22:29:21 +0100

    Add serialization benchmarks

---
+++ bench/encoding/deserializer.bench.ts
@@ -0,0 +1,21 @@
+import { Suite } from "benchmark";
+import { Deserializer, Serializer } from "../../src";
+
+export default function (suite: Suite) {
+	const data = Serializer.serialize(s => {
+		s.uint8(42);
+		s.uint16(0x1234);
+		s.uint32le(0x11223344);
+		s.prefixedUTF8(s.uint16, "Hello World!");
+		s.float64(-42.7);
+	});
+
+	suite.add(`${data.byteLength} arbitrary bytes`, () => {
+		const d = new Deserializer(data);
+		d.uint8();
+		d.uint16();
+		d.uint32le();
+		d.utf8(d.uint16());
+		d.float64();
+	});
+}
---
+++ bench/encoding/serializer.bench.ts
@@ -0,0 +1,24 @@
+import { Suite } from "benchmark";
+import { Serializer } from "../../src";
+
+export default function (suite: Suite) {
+	function serialize() {
+		const s = new Serializer();
+		s.uint8(42);
+		s.uint16(0x1234);
+		s.uint32le(0x11223344);
+		s.prefixedUTF8(s.uint16, "Hello World!");
+		s.float64(-42.7);
+		return s.serialize();
+	}
+
+	suite.add(`${serialize().byteLength} arbitrary bytes`, serialize);
+
+	const parts = 1000;
+	suite.add(`${parts} uint8 parts`, () => {
+		const serializer = new Serializer();
+		for (let i = 0; i < parts; i++) {
+			serializer.uint8(0x42);
+		}
+	});
+}
